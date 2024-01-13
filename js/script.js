d2108f8003ed6f171708d4b16b97cf784828ba5b
Lucas <lucasvw89@gmail.com>
2024-01-13 15:34:39 -0300

    basic features

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-function teste(event) {
-  document.getElementById("X").innerHTML = event.pageX;
-  document.getElementById("Y").innerHTML = event.pageY;
-}
+light = document.getElementById("light");
+
+// follow mouse
+document.addEventListener("mousemove", function (event) {
+  x = event.clientX;
+  y = event.clientY;
+  light.style.left = x + "px";
+  light.style.top = y + "px";
+})
+
+// initialize light
+document.addEventListener("mouseenter", function (event) {
+  light.style.width = 40 + "vh";
+  light.style.height = 40 + "vh";
+})
+
+// remove light
+document.addEventListener("mouseleave", function (event) {
+  light.style.width = 0 + "vh";
+  light.style.height = 0 + "vh";
+})
