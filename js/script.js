7a5be0301e3283ee8710396f2b471870b292eb1a
Lucas <lucasvw89@gmail.com>
2024-01-15 13:57:24 -0300

    better mouse tracker

--- js/script.js
+++ js/script.js
@@ -4,18 +4,30 @@ light = document.getElementById("light");
 document.addEventListener("mousemove", function (event) {
   x = event.clientX;
   y = event.clientY;
-  light.style.left = x + "px";
-  light.style.top = y + "px";
+  light.animate({
+    left: x + "px",
+    top: y + "px"
+  }, { duration: 3000, fill: "forwards" })
 })
 
 // initialize light
 document.addEventListener("mouseenter", function (event) {
-  light.style.width = 40 + "vh";
-  light.style.height = 40 + "vh";
+  x = event.clientX;
+  y = event.clientY;
+  light.animate({
+    left: x + "px",
+    top: y + "px"
+  }, { duration: 0, fill: "forwards"})
+  light.animate({
+    width: 50 + "vh",
+    height: 40 + "vh"
+  }, { duration: 500, fill: "forwards" })
 })
 
 // remove light
 document.addEventListener("mouseleave", function (event) {
-  light.style.width = 0 + "vh";
-  light.style.height = 0 + "vh";
+  light.animate({
+    width: 0 + "vh",
+    height: 0 + "vh",
+  }, { duration: 500, fill: "forwards" })
 })
